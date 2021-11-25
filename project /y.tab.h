1a4e6a44f997045858eaf6432a310c6d2e3302cd
Divi Gnanesh <gnanesh_divi@srmap.edu.in>
2021-11-25 17:01:18 +0530

    Project uploaded

---
+++ project /y.tab.h
@@ -0,0 +1,89 @@
+/* A Bison parser, made by GNU Bison 2.3.  */
+
+/* Skeleton interface for Bison's Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
+   Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     NUMBER = 258,
+     INCLUDE = 259,
+     LT = 260,
+     GT = 261,
+     HEADER = 262,
+     VOID = 263,
+     MAINFUNC = 264,
+     COUT = 265,
+     STRING = 266,
+     INT = 267,
+     ID = 268,
+     ASSIGN = 269,
+     IF = 270,
+     ELSE = 271,
+     WHILE = 272,
+     FOR = 273
+   };
+#endif
+/* Tokens.  */
+#define NUMBER 258
+#define INCLUDE 259
+#define LT 260
+#define GT 261
+#define HEADER 262
+#define VOID 263
+#define MAINFUNC 264
+#define COUT 265
+#define STRING 266
+#define INT 267
+#define ID 268
+#define ASSIGN 269
+#define IF 270
+#define ELSE 271
+#define WHILE 272
+#define FOR 273
+
+
+
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef int YYSTYPE;
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+# define YYSTYPE_IS_TRIVIAL 1
+#endif
+
+extern YYSTYPE yylval;
+
